cc61aec443996828f80c98b5ced3194c1b2ea865
M2Sky <122134945+M2Sky@users.noreply.github.com>
2024-04-04 14:34:44 -0400

    Update Evade Autofarm Combined.lua

--- Evade Autofarm Combined.lua
+++ Evade Autofarm Combined.lua
@@ -15,13 +15,22 @@ if not success then
     warn("Evade Autofarm script failed to execute:", error)
 end
 
-wait(400)
+local Player = game.Players.LocalPlayer    
+local Http = game:GetService("HttpService")
+local TPS = game:GetService("TeleportService")
+local Api = "https://games.roblox.com/v1/games/"
 
-success, error = executeScript(game:HttpGet("https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua"))
-if not success then
-    warn("Server hop script failed to execute:", error)
+local _place,_id = game.PlaceId, game.JobId
+local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=10"
+function ListServers(cursor)
+   local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+   return Http:JSONDecode(Raw)
 end
 
-if module and module.Teleport then
-    module:Teleport(game.PlaceId)
+time_to_wait = 250
+while wait(time_to_wait) do
+   Player.Character.HumanoidRootPart.Anchored = true
+   local Servers = ListServers()
+   local Server = Servers.data[math.random(1,#Servers.data)]
+   TPS:TeleportToPlaceInstance(_place, Server.id, Player)
 end
